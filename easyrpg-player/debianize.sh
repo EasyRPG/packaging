726d3eedbe38be6f11557743eb47475b09ba299b
Carsten Teibes <dev@f4ke.de>
2020-04-28 01:58:26 +0200

    Update debhelper version, supports parallel builds Split out xUbuntu16.04 package with older standard, to keep LTS support.

--- easyrpg-player/debianize.sh
+++ easyrpg-player/debianize.sh
@@ -1,32 +1,64 @@
-#!/bin/sh
+#!/bin/bash
 
-DEBVER=`grep '^Version' easyrpg-player.dsc | tr -cd '[0-9\.\-]'`
+DEBVER=$(grep '^Version' easyrpg-player.dsc | tr -cd '[0-9\.\-]')
 DEBTAR="easyrpg-player_${DEBVER}.debian.tar.xz"
+ORIGVER=$(echo ${DEBVER} | cut -d- -f1)
+ORIGTAR="easyrpg-player_${ORIGVER}.orig.tar.xz"
+
+function add_checksums() {
+	local DSCFILE=$1
+	local ORIGSIZE=$(stat -c %s ${ORIGTAR})
+	local DEBSIZE=$(stat -c %s ${DEBTAR})
+
+	local origsha256=$(sha256sum ${ORIGTAR} | cut -d' ' -f1)
+	local debsha256=$(sha256sum ${DEBTAR} | cut -d' ' -f1)
+	local origsha1=$(sha1sum ${ORIGTAR} | cut -d' ' -f1)
+	local debsha1=$(sha1sum ${DEBTAR} | cut -d' ' -f1)
+	local origmd5=$(md5sum ${ORIGTAR} | cut -d' ' -f1)
+	local debmd5=$(md5sum ${DEBTAR} | cut -d' ' -f1)
+
+	echo " ${origsha256} ${ORIGSIZE} ${ORIGTAR}" > checksums.txt
+	echo " ${debsha256} ${DEBSIZE} ${DEBTAR}" >> checksums.txt
+	echo "Checksums-Sha1:" >> checksums.txt
+	echo " ${origsha1} ${ORIGSIZE} ${ORIGTAR}" >> checksums.txt
+	echo " ${debsha1} ${DEBSIZE} ${DEBTAR}" >> checksums.txt
+	echo "Files:" >> checksums.txt
+	echo " ${origmd5} ${ORIGSIZE} ${ORIGTAR}" >> checksums.txt
+	echo " ${debmd5} ${DEBSIZE} ${DEBTAR}" >> checksums.txt
+
+	local lead='^Checksums-Sha256:$'
+	sed -i "/$lead/,$ { /$lead/{p; r checksums.txt
+        }; d }" ${DSCFILE}
+
+	rm checksums.txt
+}
+
+echo "Copying source archive:"
+cp "easyrpg-player-${ORIGVER}.tar.xz" $ORIGTAR
 
 echo "Creating ${DEBTAR}:"
-
 tar -cvJf "${DEBTAR}" --exclude='*.ex' debian
 
 echo "Updating checksums..."
+add_checksums easyrpg-player.dsc
 
-ORIGVER=`echo ${DEBVER} | cut -d- -f1`
-ORIGTAR="easyrpg-player-${ORIGVER}.tar.xz"
-ORIGSIZE=`stat -c %s ${ORIGTAR}`
+COMPATDISTRO="xUbuntu_16.04"
+echo "Generating compat package for ${COMPATDISTRO} Xenial"
 
-DEBSIZE=`stat -c %s ${DEBTAR}`
+sed -i'.bak' 's/10/9/' debian/compat
+sed -i'.bak' 's/debhelper (>= 10)/debhelper (>= 9)/' debian/control
+sed -e 's/debhelper (>= 10)/debhelper (>= 9)/' \
+  -e "s/${DEBVER}/&xenial/" \
+  easyrpg-player.dsc > "easyrpg-player-${COMPATDISTRO}.dsc"
 
-echo " `sha256sum ${ORIGTAR} | cut -d' ' -f1` ${ORIGSIZE} easyrpg-player_${ORIGVER}.orig.tar.xz" > checksums.txt
-echo " `sha256sum ${DEBTAR} | cut -d' ' -f1` ${DEBSIZE} ${DEBTAR}" >> checksums.txt
-echo "Checksums-Sha1:" >> checksums.txt
-echo " `sha1sum ${ORIGTAR} | cut -d' ' -f1` ${ORIGSIZE} easyrpg-player_${ORIGVER}.orig.tar.xz" >> checksums.txt
-echo " `sha1sum ${DEBTAR} | cut -d' ' -f1` ${DEBSIZE} ${DEBTAR}" >> checksums.txt
-echo "Files:" >> checksums.txt
-echo " `md5sum ${ORIGTAR} | cut -d' ' -f1` ${ORIGSIZE} easyrpg-player_${ORIGVER}.orig.tar.xz" >> checksums.txt
-echo " `md5sum ${DEBTAR} | cut -d' ' -f1` ${DEBSIZE} ${DEBTAR}" >> checksums.txt
+DEBVER="${DEBVER}xenial"
+DEBTAR="easyrpg-player_${DEBVER}.debian.tar.xz"
 
-lead='^Checksums-Sha256:$'
-sed -i "/$lead/,$ { /$lead/{p; r checksums.txt
-        }; d }"  easyrpg-player.dsc
+echo "Creating ${DEBTAR}:"
+tar -cvJf "${DEBTAR}" --exclude='*.ex' debian
 
-rm checksums.txt
-cp "easyrpg-player-${ORIGVER}.tar.xz" "easyrpg-player_${ORIGVER}.orig.tar.xz"
+echo "Updating checksums..."
+add_checksums "easyrpg-player-${COMPATDISTRO}.dsc"
+
+mv debian/compat.bak debian/compat
+mv debian/control.bak debian/control
--- easyrpg-player/easyrpg-player-xUbuntu_16.04.dsc
+++ easyrpg-player/easyrpg-player-xUbuntu_16.04.dsc
@@ -2,19 +2,19 @@ Format: 3.0 (quilt)
 Source: easyrpg-player
 Binary: easyrpg-player
 Architecture: any
-Version: 0.6.2-1
+Version: 0.6.2-1xenial
 Maintainer: carstene1ns <dev@ f4ke .de>
 Homepage: https://easyrpg.org
 Standards-Version: 3.9.6
-Build-Depends: debhelper (>= 10), pkg-config, liblcf-dev, libsdl2-dev, libsdl2-mixer-dev, libpixman-1-dev, libpng-dev, libfreetype6-dev, libogg-dev, libvorbis-dev, libspeexdsp-dev, libsndfile-dev
+Build-Depends: debhelper (>= 9), pkg-config, liblcf-dev, libsdl2-dev, libsdl2-mixer-dev, libpixman-1-dev, libpng-dev, libfreetype6-dev, libogg-dev, libvorbis-dev, libspeexdsp-dev, libsndfile-dev
 Package-List:
  easyrpg-player deb extra
 Checksums-Sha256:
  3550200c4b7c42fa7bd5774de5016b48d5660cc33d1614f174f474702ee253b9 4202508 easyrpg-player_0.6.2.orig.tar.xz
- 5b41c4e6c76346cb70f88eedadb0818677de840b24fccbf3f254b8571e971f0a 2952 easyrpg-player_0.6.2-1.debian.tar.xz
+ 9b49f16a9afbe012f3a3c47d9943fe2d07b3cad1a77cf1ad16b3dffdc5588a14 2988 easyrpg-player_0.6.2-1xenial.debian.tar.xz
 Checksums-Sha1:
  423f7e25eaf45b61c4050385d6aa1b1b433bbdb4 4202508 easyrpg-player_0.6.2.orig.tar.xz
- 818ce4ef54658df48edcdac20ae94eba37f5fe6b 2952 easyrpg-player_0.6.2-1.debian.tar.xz
+ 019d87aa70b934b3a50ddb1c37ad9ce83d2e0212 2988 easyrpg-player_0.6.2-1xenial.debian.tar.xz
 Files:
  e4de29261660c04893b2a8d8cedb932b 4202508 easyrpg-player_0.6.2.orig.tar.xz
- 7db2b61e126f17d02612ba6ad7833e2d 2952 easyrpg-player_0.6.2-1.debian.tar.xz
+ b491ef1171e33426b539203e56ade7c1 2988 easyrpg-player_0.6.2-1xenial.debian.tar.xz
