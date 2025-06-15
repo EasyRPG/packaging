19c06dacab3371f340c6d390d355ba6bbfbadf16
Carsten Teibes <dev@f4ke.de>
2025-06-16 00:59:01 +0200

    OBS: update distribution graph

--- OBS/_scripts/distri-graph.sh
+++ OBS/_scripts/distri-graph.sh
@@ -16,6 +16,7 @@ graph distributions {
 	i586;
 	armv7l;
 	aarch64;
+	ppc64le;
 
 	# distris
 
@@ -23,7 +24,6 @@ graph distributions {
 
 	node[color=red];
 	Debian;
-	d10 [label="10"];
 	d11 [label="11"];
 	d12 [label="12"];
 	dt  [label="testing"];
@@ -31,23 +31,24 @@ graph distributions {
 
 	node[color="#B3446C"];
 	Raspbian;
-	r10 [label="10"];
 	r11 [label="11"];
 	r12 [label="12"];
 
 	node[color=green];
 	openSUSE;
-	s154 [label="Leap 15.4"];
-	s155 [label="Leap 15.5"];
 	s156 [label="Leap 15.6"];
+	s160 [label="Leap 16.0"];
+	sfa  [label="Factory ARM"]
+	sfp  [label="Factory PowerPC"]
+	sl   [label="Slowroll"];
 	st   [label="Tumbleweed"];
 
 	node[color=orange];
 	xUbuntu;
-	u2004 [label="20.04"];
 	u2204 [label="22.04"];
-	u2304 [label="23.04"];
-	u2310 [label="23.10"];
+	u2404 [label="24.04"];
+	u2410 [label="24.10"];
+	u2504 [label="25.04"];
 
 	node[color=blue];
 	Arch;
@@ -57,35 +58,35 @@ graph distributions {
 	Arch -- x86_64
 
 	edge[color=red];
-	d10 -- { x86_64 i586 aarch64 };
 	d11 -- { x86_64 i586 };
 	d12 -- { x86_64 i586 };
 	dt -- x86_64;
 	du -- x86_64;
 
 	edge[color="#B3446C"];
-	r10 -- armv7l;
 	r11 -- armv7l;
 	r12 -- armv7l;
 
 	edge[color=green];
-	s154 -- x86_64;
-	s155 -- x86_64;
 	s156 -- x86_64;
+	s160 -- x86_64;
+	sfa -- aarch64;
+	sfp -- ppc64le;
+	sl -- { x86_64 i586 };
 	st -- { x86_64 i586 };
 
 	edge[color=orange];
-	u2004 -- x86_64;
 	u2204 -- x86_64;
-	u2304 -- x86_64;
-	u2310 -- x86_64;
+	u2404 -- x86_64;
+	u2410 -- x86_64;
+	u2504 -- x86_64;
 
 	# order!
 
 	edge [style=dotted, color=gray];
-	Debian -- {d10 d11 d12 dt du}
-	Raspbian -- {r10 r11 r12}
-	openSUSE -- {s154 s155 s156 st}
-	xUbuntu -- {u2004 u2204 u2304 u2310}
+	Debian -- {d11 d12 dt du}
+	Raspbian -- {r11 r12}
+	openSUSE -- {s156 s160 sfa sfp sl st}
+	xUbuntu -- {u2204 u2404 u2410 u2504}
 }
 EOD
